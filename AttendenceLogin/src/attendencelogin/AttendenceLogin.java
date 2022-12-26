0906e7a4ee83bb1c7d7722c110c76d70610133a8
FAIZAN ALI IQBAL ALI <67515374+Faizan2907@users.noreply.github.com>
2022-12-26 19:35:35 +0530

    Add files via upload

---
+++ AttendenceLogin/src/attendencelogin/AttendenceLogin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendencelogin;
+
+/**
+ *
+ * @author Mr. Faizu
+ */
+public class AttendenceLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        RegForm ob = new RegForm();
+        ob.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
